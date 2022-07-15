69d90ee73b840602d73483b132c9c8ab87a1fb29
haoadoreorange <github@h4o.dev>
2022-07-15 01:30:56 -0400

    Install script pull if already exist
    Signed-off-by: haoadoreorange <github@h4o.dev>

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 INSTALL_DIR="${1:-"$HOME"/.deven}"
 
 if [ ! -d "$INSTALL_DIR" ]; then
     mkdir -p "$INSTALL_DIR"
     git clone https://github.com/haoadoreorange/deven "$INSTALL_DIR"
-    echo "Download deven to $INSTALL_DIR successfully"
+    echo -e "${GREEN}Download deven to $INSTALL_DIR successfully${NC}"
+else
+    (
+        cd "$INSTALL_DIR"
+        git pull
+    )
 fi
 
 chmod +x "$INSTALL_DIR"/deven.sh
 mkdir -p "$HOME"/.local/bin
 ln -s "$INSTALL_DIR"/deven.sh "$HOME"/.local/bin/deven
-echo "Softlink deven binary to $HOME/.local/bin successfully"
-echo "deven installed successfully, you might need to add ~/.local/bin/ to PATH to use it"
+echo -e "${GREEN}Softlink deven binary to $HOME/.local/bin successfully${NC}"
+echo -e "${GREEN}deven installed successfully, you might need to add ~/.local/bin/ to PATH to use it${NC}"
